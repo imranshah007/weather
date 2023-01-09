5938445d256d122d097ba136cf930bb992ab1d35
Imran Shah <is9149933764@gmail.com>
2023-01-09 22:41:43 +0530

    commit

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
+
 let i =0;
+
+
 function weather() {
- if(i<4){
+ 
+
+    
+
+
+if(i<4){
    let Cities = document.getElementsByClassName("sub")
-let city = `${Cities[i].innerHTML}`
-fetch(`https://python3-dot-parul-arena-2.appspot.com/test?cityname=${city}`)
+   
+   console.log(Cities[0]);
+
+   
+   
+   
+   let city = `${Cities[i].innerHTML}`
+
+
+   
+   
+   fetch(`https://python3-dot-parul-arena-2.appspot.com/test?cityname=${city}`)
    .then(res => {
       return res.json();  
    })
    .then(data => {
+    
+     
+   //get Body
+
     let Tbody = document.getElementById("TBody")
-   let tr =  document.createElement("tr")
-   let td = document.createElement("td")
+     
+   // create row 
+    let tr =  document.createElement("tr")
+
+
+   //create td
+    let td = document.createElement("td")
     td.innerHTML = city;
+    
 
     let td2 = document.createElement("td")
     td2.innerHTML = data.description;
@@ -45,14 +73,32 @@ fetch(`https://python3-dot-parul-arena-2.appspot.com/test?cityname=${city}`)
     tr.appendChild(td4)
     tr.appendChild(td5)
     tr.appendChild(td6)
+    
+    // append row to body
+
     Tbody.appendChild(tr)
-btn12.addEventListener("click", () => {remove(tr)} )
-    (Cities[i-1]).classList.add("pg");
-let nodata = document.getElementById("nodata");
+
+
+
+
+    btn12.addEventListener("click", () => {remove(tr)} )
+    
+    console.log(td6);
+    
+    
+   //side-bar-functionality
+   (Cities[i-1]).classList.add("pg");
+
+  //nodata remove
+   
+  let nodata = document.getElementById("nodata");
 
   nodata.classList.add("kg");
 
    })
+
+
+  
 }else{
    return
 }
@@ -60,14 +106,35 @@ let nodata = document.getElementById("nodata");
 
 
 }
+
+//save
+
+
 function remove(e) {
     
   e.remove()
     
 }
+
+
+
 function Search() {
    let input1 = document.getElementById("input").value
-let tr = document.getElementsByTagName("tr")
+
+   // let td = document.getElementsByTagName("td")
+
+   let tr = document.getElementsByTagName("tr")
+
+
+
+ //  console.log(input1);
+ //  console.log(tr.parentElement);
+ //  console.log(tr[0].innerHTML)
+  
+//if(input1===td[0].innerHTML) {
+ // tr[1].classList.add("bg");
+//}
+
 
 if(input1==="London"){
    tr[1].classList.add("bg");
@@ -80,5 +147,9 @@ if(input1==="London"){
 } else{
    return
 }
+
+
+
+
 
 }
